d4aeeb5596b33e3cd7f8b956d4efa17857134bee
Luke Melvin <luke@lucasmelvin.com>
2020-04-06 22:46:27 -0500

    Initial checkin

---
+++ AdventureWorks.GraphQL.App/Models/EmployeeDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventureWorks.GraphQL.App.Models
+{
+    public partial class EmployeeDemo
+    {
+        public int EmployeeId { get; set; }
+        public string LoginId { get; set; }
+        public int? ManagerId { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
